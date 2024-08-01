61e6fb14d4cbe855bb954b7041b410b013648574
Ashraful-Ahsan <ahsanxt@gmail.com>
2024-08-01 08:43:14 +0600

    add welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
